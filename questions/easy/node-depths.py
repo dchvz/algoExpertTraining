d643e9483c1bdabfcba82b0f27c0574cd74a377f
Diego Alejandro Chávez Soria <diego.chavezsoria@gmail.com>
2025-07-23 19:11:37 -0400

    Add prompt for the first problem

---
+++ questions/easy/node-depths.py
@@ -0,0 +1,27 @@
+# Node Depths 🟢
+# The distance between a node in a Binary tree and the tree's root node is called the node's depth.
+# Write a function that takes a Binary Tree and returns the sum of its nodes' depths.
+# Each BinaryTree node has a `value` property, a `left` property pointing to the left child node, and a `right` property pointing to the right child node. The root node's depth is 0.
+
+# Sample Input
+# tree =
+#         1
+#        / \
+#       2   3
+#      / \
+#     4   8
+#    / \
+#   5   6
+## Sample Output: 8
+## Explanation: The depths of the nodes are as follows:
+# - Node 1 (root): depth 0
+# - Node 2: depth 1
+# - Node 3: depth 1
+# - Node 4: depth 2
+# - Node 8: depth 2
+# - Node 5: depth 3
+# - Node 6: depth 3
+
+# Solution...
+
+
