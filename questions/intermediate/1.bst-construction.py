fc6370864b49d024199bb3c0fe3d5c375254dda6
Diego Chavez <diegochavezsoria@gmail.com>
2025-09-12 20:25:40 -0400

    Fix indentation for bst construction problem

--- questions/intermediate/1.bst-construction.py
+++ questions/intermediate/1.bst-construction.py
@@ -41,48 +41,47 @@ class BST:
     return self
 
   def contains(self, value):
-      currentNode = self
-      while currentNode is not None:
-        print(currentNode.value, value)
-        if currentNode.value == value:
-          return True
-        elif currentNode.value > value:
-          currentNode = currentNode.left
-        else:
-          currentNode = currentNode.right
-      return False
+    currentNode = self
+    while currentNode is not None:
+      print(currentNode.value, value)
+      if currentNode.value == value:
+        return True
+      elif currentNode.value > value:
+        currentNode = currentNode.left
+      else:
+        currentNode = currentNode.right
+    return False
 
   def remove(self, value, parentNode = None):
-      currentNode = self
-      while currentNode is not None:
-        if value < currentNode.value:
-          parentNode = currentNode
-          currentNode = currentNode.left
-        elif value > currentNode.value:
-          parentNode = currentNode
-          currentNode = currentNode.right
-        else:
-          if currentNode.left is not None and currentNode.right is not None:
-            currentNode.value = currentNode.right.getMinValue()
-            currentNode.right.remove(currentNode.value, currentNode)
-          elif parentNode is None:
-            if currentNode.left is not None:
-              currentNode.value = currentNode.left.value
-              currentNode.right = currentNode.left.right
-              currentNode.left = currentNode.left.left
-            elif currentNode.right is not None:
-              currentNode.value = currentNode.right.value
-              currentNode.left = currentNode.right.left
-              currentNode.right = currentNode.right.right
+    currentNode = self
+    while currentNode is not None:
+      if value < currentNode.value:
+        parentNode = currentNode
+        currentNode = currentNode.left
+      elif value > currentNode.value:
+        parentNode = currentNode
+        currentNode = currentNode.right
+      else:
+        if currentNode.left is not None and currentNode.right is not None:
+          currentNode.value = currentNode.right.getMinValue()
+          currentNode.right.remove(currentNode.value, currentNode)
+        elif parentNode is None:
+          if currentNode.left is not None:
+            currentNode.value = currentNode.left.value
+            currentNode.right = currentNode.left.right
+            currentNode.left = currentNode.left.left
+          elif currentNode.right is not None:
+            currentNode.value = currentNode.right.value
+            currentNode.left = currentNode.right.left
+            currentNode.right = currentNode.right.right
 
-          elif parentNode.left == currentNode:
-            parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+        elif parentNode.left == currentNode:
+          parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
 
-          elif parentNode.right == currentNode:
-            parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
-
-          break
-      return self
+        elif parentNode.right == currentNode:
+          parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+        break
+    return self
 
   def getMinValue(self):
     currentNode = self
